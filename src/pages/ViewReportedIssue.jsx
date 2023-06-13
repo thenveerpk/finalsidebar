7888e8a8547cbf7d23dbe85deae502076386765d
Angelrose19 <angelroseinchodikkaran@gmail.com>
2023-06-13 23:50:24 +0530

    new files

--- src/pages/ViewReportedIssue.jsx
+++ src/pages/ViewReportedIssue.jsx
@@ -3,7 +3,97 @@ import React from 'react';
 const ViewReportedIssue = () => {
     return (
         <div>
-            <h1>ViewReportedIssue page</h1>
+            <div className='staff' >
+                <h1 className='h2'> Rank Lists</h1>
+            
+                
+                        
+                </div>
+                <div className='z'>
+
+                </div>
+            <table  border={5} width={1200} cellPadding={20} >
+                <tr height={70}>
+                    <th>No.</th>
+                    <th>Issue</th>
+                    <th>Reported By</th>
+                    <th>Status</th>
+                    
+                </tr>
+                <tr height={70}>
+                    <td>1</td>
+                    <td>Login Issue</td>
+                    <td>Ihjas</td>
+                    <td>
+                        <select>
+                            <option value="option1">Open</option>
+                            <option value="option2">Close</option>
+                            
+                        </select>
+                    </td>
+                    
+                </tr>
+                <tr height={70}>
+                <td>1</td>
+                    <td>Login Issue</td>
+                    <td>Ihjas</td>
+                    <td>
+                        <select>
+                            <option value="option1">Open</option>
+                            <option value="option2">Close</option>
+                            
+                        </select>
+                    </td>
+                </tr>
+                <tr height={70}>
+                <td>1</td>
+                    <td>Login Issue</td>
+                    <td>Ihjas</td>
+                    <td>
+                        <select>
+                            <option value="option1">Open</option>
+                            <option value="option2">Close</option>
+                            
+                        </select>
+                    </td>
+                </tr>
+                <tr height={70}>
+                <td>1</td>
+                    <td>Login Issue</td>
+                    <td>Ihjas</td>
+                    <td>
+                        <select>
+                            <option value="option1">Open</option>
+                            <option value="option2">Close</option>
+                            
+                        </select>
+                    </td>
+                </tr>
+                <tr height={70}>
+                <td>1</td>
+                    <td>Login Issue</td>
+                    <td>Ihjas</td>
+                    <td>
+                        <select>
+                            <option value="option1">Open</option>
+                            <option value="option2">Close</option>
+                            
+                        </select>
+                    </td>
+                </tr>
+                <tr height={70}>
+                <td>1</td>
+                    <td>Login Issue</td>
+                    <td>Ihjas</td>
+                    <td>
+                        <select>
+                            <option value="option1">Open</option>
+                            <option value="option2">Close</option>
+                            
+                        </select>
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 };
